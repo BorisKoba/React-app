9dcaf55fde26635b72d0bfa05478aea52f54964f
BorisKoba <browi@mail.ru>
2023-03-05 17:09:58 +0300

    HW#31

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,30 +1,24 @@
 import React from "react"
-export const Timer: React.FC = () =>{
-     const styles: React.CSSProperties = {backgroundColor:"lightblue",
-fontSize:"2em"}  ;
- setTimeout(tic, 1000);
-setTimeout(colorTime, 10000);
-const [time, setTime] = React.useState(new Date());
-const [color, setColor] = React.useState(styles)
+export const Timer: React.FC = () => {
+      const styles: React.CSSProperties = {
+            backgroundColor: "blue",
+            fontSize: "2em"
+      
+      };
+      setTimeout(tic, 1000);
+      setInterval(colorTime, 10000);
+      const [time, setTime] = React.useState(new Date());
+      const [color, setColor] = React.useState(styles);
 
-function tic(){
-       setTime (new Date())
-}
-function colorTime(){
-        setColor(styles)
-        
-              if(styles.backgroundColor ="lightblue"){
-                styles.backgroundColor="red";
-        }
+      function tic() {
+            setTime(new Date());
+      }
+      function colorTime() {
+            setColor(styles)
+            styles.backgroundColor = "blue"?"red":"blue";     
+      }
 
-              else{styles.backgroundColor ="lightblue";
-        }
-
-              
-              return styles;
-        }
-
-        return <div>
-                <p style={color}>{time.toLocaleTimeString()}</p>
-        </div>
+      return <div>
+            <p style={color}>{time.toLocaleTimeString()}</p>
+      </div>
 }
